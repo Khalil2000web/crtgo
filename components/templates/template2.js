256fca73d452ebd6959a739c8e839be1d5fdff10
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2026-03-18 22:09:06 +0200

    Update template2.js

--- components/templates/template2.js
+++ components/templates/template2.js
@@ -1,14 +1,35 @@
 export default function Template2({ data }) {
   return (
     <div className="bg-black text-white p-10">
-      <h1 className="text-5xl">{data.name}</h1>
 
-      {data.menu.map((item) => (
-        <div key={item.name} className="flex justify-between">
-          <span>{item.name}</span>
-          <span>${item.price}</span>
-        </div>
-      ))}
+      <h1 className="text-5xl mb-8">
+        {data.name}
+      </h1>
+
+      {data.sections.map((section, i) => {
+
+        if (section.type === "menu") {
+          return (
+            <div key={i} className="mb-10">
+
+              <h2 className="text-3xl mb-6">
+                {section.title}
+              </h2>
+
+              {section.items.map((item) => (
+                <div key={item.name} className="flex justify-between border-b border-gray-700 py-2">
+                  <span>{item.name}</span>
+                  <span>${item.price}</span>
+                </div>
+              ))}
+
+            </div>
+          )
+        }
+
+        return null
+      })}
+
     </div>
   )
 }
