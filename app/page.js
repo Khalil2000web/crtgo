b767d1d388f7c35bbb0bcfde204b43159108b1d5
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2026-03-18 17:37:39 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,4 @@
 // app/page.js
-import Link from "next/link"
-
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-
-      <h1 className="text-4xl font-bold mb-6">
-        My Website Builder
-      </h1>
-
-      <Link
-        href="/login"
-        className="bg-white text-black px-6 py-3 rounded-lg"
-      >
-        Login
-      </Link>
-
-    </div>
-  )
+  return null;
 }
