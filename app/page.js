e08bc168dd3d532b0496ed84ee8134fb42d3ff75
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2026-03-19 08:33:55 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,28 @@
 // app/page.js
-export default function Home() {
-  return null;
+// app/page.js
+import { Noto_Sans_Arabic, Zain } from "next/font/google";
+
+const notoArabic = Noto_Sans_Arabic({
+  weight: ["100","200","300","400","500","600","700","800","900"],
+  subsets: ["arabic"],
+  display: "swap",
+});
+
+const zain = Zain({
+  weight: ["200","300","400","700","800","900"],
+  style: ["normal", "italic"],
+  subsets: ["arabic"],
+  display: "swap",
+});
+
+export default function Page() {
+  return (
+    <div
+      className={`flex min-h-screen items-center justify-center bg-gray-50 ${notoArabic.className}`}
+    >
+      <h1 className="text-5xl text-center">
+        مرحبًا   
+      </h1>
+    </div>
+  );
 }
